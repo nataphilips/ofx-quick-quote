12e75df524ab4a60c7e0e753c05ce37c04364476
Natalia Filippova <fnd.natalia@gmail.com>
2022-10-12 23:20:45 +1100

    add recoil to the app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import AppStackNavigator from './src/navigation/AppStackNavigator';
+import React from 'react'
+import { SafeAreaView, StatusBar, useColorScheme } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { RecoilRoot } from 'recoil'
+import AppStackNavigator from './src/navigation/AppStackNavigator'
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark'
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : 'white',
-  };
+  }
 
   return (
-    <SafeAreaView style={[backgroundStyle, {flex: 1}]}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <AppStackNavigator />
-    </SafeAreaView>
-  );
-};
+    <RecoilRoot>
+      <SafeAreaView style={[backgroundStyle, { flex: 1 }]}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <AppStackNavigator />
+      </SafeAreaView>
+    </RecoilRoot>
+  )
+}
 
-export default App;
+export default App
