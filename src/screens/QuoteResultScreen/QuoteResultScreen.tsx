5ccd1727cb08143908a818d7b5ff0f157456606c
Natalia Filippova <fnd.natalia@gmail.com>
2022-10-13 14:09:28 +1100

    start a new quote button and function

--- src/screens/QuoteResultScreen/QuoteResultScreen.tsx
+++ src/screens/QuoteResultScreen/QuoteResultScreen.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {ScrollView, Text} from 'react-native';
-import {useRecoilState} from 'recoil';
+import {useRecoilState, useResetRecoilState} from 'recoil';
 import {quickQuoteState} from '../../services/quickQuote';
+import Button from '../../components/Button';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {AppStackParamList} from '../../navigation/AppStackNavigator';
 
 const QuoteResultScreen = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<AppStackParamList>>();
   const [quote] = useRecoilState(quickQuoteState);
+  const resetQuote = useResetRecoilState(quickQuoteState);
+
+  const onStartNewQuote = useCallback(() => {
+    resetQuote();
+    navigation.navigate('QuoteForm');
+  }, [navigation, resetQuote]);
 
   return (
     <ScrollView>
@@ -13,6 +25,7 @@ const QuoteResultScreen = () => {
 
       <Text>To {quote.to.currency}</Text>
       <Text>To {quote.to.amount}</Text>
+      <Button label={'Start new quote'} onPress={onStartNewQuote} />
     </ScrollView>
   );
 };
