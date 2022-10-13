9a560878734ac8f15ac06b726a93a4b840700b84
Natalia Filippova <fnd.natalia@gmail.com>
2022-10-13 20:56:06 +1100

    fixing component tests after recoil and navigation

--- src/screens/QuoteFormScreen/QuoteFormScreen.test.tsx
+++ src/screens/QuoteFormScreen/QuoteFormScreen.test.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {render, RenderAPI} from '@testing-library/react-native';
 import QuoteFormScreen from './QuoteFormScreen';
+import {RecoilRoot} from 'recoil';
 
 jest.mock('react-native-select-dropdown', () => 'SelectDropdown');
+
+const mockedNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => mockedNavigate,
+}));
 
 describe('QuoteFormScreen component', () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
+
+  const wrapper = (component: React.ReactElement): React.ReactElement => {
+    return <RecoilRoot>{component}</RecoilRoot>;
+  };
 
   const elements = {
     firstNameFieldPlaceholder: (api: RenderAPI) =>
@@ -18,22 +28,22 @@ describe('QuoteFormScreen component', () => {
   };
 
   it('renders first name form field', () => {
-    const component = render(<QuoteFormScreen />);
+    const component = render(wrapper(<QuoteFormScreen />));
     expect(elements.firstNameFieldPlaceholder(component)).toBeDefined();
   });
 
   it('renders from currency picker label', () => {
-    const component = render(<QuoteFormScreen />);
+    const component = render(wrapper(<QuoteFormScreen />));
     expect(elements.fromCurrencyLabel(component)).toBeDefined();
   });
 
   it('renders to currency picker label', () => {
-    const component = render(<QuoteFormScreen />);
+    const component = render(wrapper(<QuoteFormScreen />));
     expect(elements.toCurrencyLabel(component)).toBeDefined();
   });
 
   it('renders to amount label', () => {
-    const component = render(<QuoteFormScreen />);
+    const component = render(wrapper(<QuoteFormScreen />));
     expect(elements.amountLabel(component)).toBeDefined();
   });
 });
